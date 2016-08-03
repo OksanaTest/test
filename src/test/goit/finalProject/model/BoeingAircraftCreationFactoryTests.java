404b8a298ae08147855ead8a0487fc3bb94baf16
Oksana <pasom@bigmir.net>
2016-08-04 03:07:49 +0400

    HomeWork

---
+++ src/test/goit/finalProject/model/BoeingAircraftCreationFactoryTests.java
@@ -0,0 +1,53 @@
+package test.goit.finalProject.model;
+
+import com.goit.finalProject.projectCode.model.Aerostat;
+import com.goit.finalProject.projectCode.model.BoeingAircraftCreationFactory;
+import com.goit.finalProject.projectCode.model.Helicopter;
+import com.goit.finalProject.projectCode.model.Plane;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+public class BoeingAircraftCreationFactoryTests {
+    private BoeingAircraftCreationFactory boeingAircraftCreationFactory = new BoeingAircraftCreationFactory();
+    private String[] arrayWithNameBoeing777 = {"Boeing 777", "Boeing 777"};
+    private String[] arrayWithNameBell47 = {"Bell 47"};
+    private String[] arrayWithNameATC07 = {"07 ATC", "07 ATC", "07 ATC"};
+
+    @Test
+    public void createPlane() {
+        List<Plane> boeing777List;
+        boeing777List = boeingAircraftCreationFactory.createPlane("Boeing 777", 2);
+        String[] arrayWithName = new String[2];
+        for (int i = 0; i < boeing777List.size(); i++){
+            arrayWithName[i] = boeing777List.get(i).getAircraftName();
+        }
+        assertEquals(2, boeing777List.size());
+        assertArrayEquals(arrayWithName, arrayWithNameBoeing777);
+    }
+    @Test
+    public void createHelicopter(){
+        List<Helicopter> bell47List;
+        bell47List = boeingAircraftCreationFactory.createHelicopter("Bell 47", 1);
+        String[] arrayWithName = new String[1];
+        for (Helicopter aBell47List : bell47List) {
+            arrayWithName[0] = aBell47List.getAircraftName();
+        }
+        assertEquals(1, bell47List.size());
+        assertArrayEquals(arrayWithName, arrayWithNameBell47);
+    }
+    @Test
+    public void crateAerostat(){
+        List<Aerostat> ATC07List;
+        ATC07List = boeingAircraftCreationFactory.crateAerostat("07 ATC", 3);
+        String[] arrayWithName = new String[3];
+        for (int i = 0; i < ATC07List.size(); i++){
+            arrayWithName[i] = ATC07List.get(i).getAircraftName();
+        }
+        assertEquals(3, ATC07List.size());
+        assertArrayEquals(arrayWithName, arrayWithNameATC07);
+    }
+}
+
