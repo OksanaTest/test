ef15eabb7ee51b31c4ab9cc2fa4d095f8a346ec2
Oksana <pasom@bigmir.net>
2016-09-01 16:13:06 +0400

    GenMethods

---
+++ src/com/goit/examplesFromShildt/generics/example05/GenMethDemo.java
@@ -0,0 +1,40 @@
+package com.goit.examplesFromShildt.generics.example05;
+
+import com.goit.examplesFromShildt.generics.example03.Stats;
+
+public class GenMethDemo {
+    //определить, содержится ли лбъект в массиве
+    static <T extends Comparable<T>, V extends T> boolean isIn(T x, V[] y){
+        for (int i = 0; i < y.length; i++) {
+            if (x.equals(y[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        //применить метод isIn для целых чисел
+        Integer inums[] = {1, 5, 6, 34, 123, -23};
+        if (isIn(23, inums)) {
+            System.out.println("inums contains 23");
+        }else System.out.println("inums does not contain 23");
+        if (isIn(123, inums)){
+            System.out.println("inums contains -123");
+        }
+        else System.out.println("inums does not contain -123");
+        //for Strings
+        String strnums[] = {"test", "frog", "cat", "dog"};
+        if (isIn("test", strnums)) {
+            System.out.println("srtnum contains test");
+        }
+        else System.out.println("srtnum does not contain test");
+        if (isIn("quaaaa", strnums)){
+            System.out.println("srtnum contains quaaaa");
+        }
+        else System.out.println("srtnum does not contain quaaaa");
+
+        }
+
+
+}
