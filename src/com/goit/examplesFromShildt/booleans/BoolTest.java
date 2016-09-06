b89b610f21f8633a4db2a4b30ffa41e17d6f8da2
Oksana <pasom@bigmir.net>
2016-09-06 17:34:40 +0400

    Boolean

---
+++ src/com/goit/examplesFromShildt/booleans/BoolTest.java
@@ -0,0 +1,17 @@
+package com.goit.examplesFromShildt.booleans;
+/*
+p. 81
+ */
+public class BoolTest {
+    public static void main(String[] args) {
+     boolean b;
+        b = false;
+        System.out.println("b = " + b);
+        b = true;
+        System.out.println("b = " + b);
+        if (b) System.out.println("b is true");
+        b = false;
+        System.out.println("b is false");
+        System.out.println("10 > 9 " + (10>9));
+    }
+}
