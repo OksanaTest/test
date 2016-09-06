378a32e349676098a4b615e9c2976dd58de367c7
Oksana <pasom@bigmir.net>
2016-09-06 17:17:36 +0400

    Char

---
+++ src/com/goit/examplesFromShildt/chars/CharDemo.java
@@ -0,0 +1,15 @@
+package com.goit.examplesFromShildt.chars;
+/*
+p.80
+ */
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1,ch2;
+
+        ch1 = 88; //X symbol <code>
+        ch2 = 'Y';
+
+        System.out.println("ch1 = " + ch1);
+        System.out.println("ch2 = " + ch2);
+    }
+}
