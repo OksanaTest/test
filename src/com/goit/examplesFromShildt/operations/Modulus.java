00b7f727e748a93fb9ac87cd2cd8ad28f7388c59
Oksana <pasom@bigmir.net>
2016-09-14 03:55:41 +0400

    Operation

---
+++ src/com/goit/examplesFromShildt/operations/Modulus.java
@@ -0,0 +1,11 @@
+package com.goit.examplesFromShildt.operations;
+
+public class Modulus {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.25;
+
+        System.out.println("x mod 11 = " + x%11);
+        System.out.println("y mod 10 = " + y%10);
+    }
+}
